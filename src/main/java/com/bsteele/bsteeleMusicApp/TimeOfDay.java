60ed0fea6f8d44fc429fff056613a98d43e9994d
pearlbob <bob@bsteele.com>
2018-01-29 14:56:48 -0800

    attempted super dev mode

---
+++ src/main/java/com/bsteele/bsteeleMusicApp/TimeOfDay.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2018 Robert Steele, bsteele.com
+ * All rights reserved.
+ */
+package com.bsteele.bsteeleMusicApp;
+
+import jsinterop.annotations.JsType;
+
+/**
+ *
+ * @author bob
+ */
+@JsType
+public class TimeOfDay
+{
+    public String timeOfDay(){
+	return "java method timeOfDay()";
+    }
+}
---
+++ src/main/java/com/bsteele/bsteeleMusicApp/shared/Note.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2018 Robert Steele, bsteele.com
+ * All rights reserved.
+ */
+package com.bsteele.bsteeleMusicApp.shared;
+
+/**
+ *
+ * @author bob
+ */
+public class Note
+{
+
+    private String lyrics;
+    public String getLyrics(){return lyrics;}
+    public double getDurationIn(){
+	return 0.0;
+    }
+    
+}
