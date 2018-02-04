649fe04892fdccf50173140b9e13fd966fbd95e2
pearlbob <bob@bsteele.com>
2018-02-04 08:53:27 -0800

    clientSong.transcribe()  finished

--- src/main/java/com/bsteele/bsteeleMusicApp/client/Section.java
+++ src/main/java/com/bsteele/bsteeleMusicApp/client/Section.java
@@ -11,15 +11,52 @@ public enum Section {
     intro("i"),
     verse("v"),
     preChorus("pc"),
-    chorus("c"),
+    chorus("c", "ch"),
     bridge("br"),
     coda("co"),
     tag("t"),
     outro("o");
 
-    Section(String abrev) {
+    /**
+     * Return length of match at start of string. Match will ignore case.
+     *
+     * @param s the string to match
+     * @return the length of the match. Zero if no match
+     */
+    public static int matchLength(String s) {
+        if (s == null) {
+            return 0;
+        }
+        s = s.substring(0, Math.min(s.length(), maxLength));
+        s = s.toLowerCase();
 
+        for (Section sec : Section.values()) {
+
+            if (s.startsWith(sec.lowerCaseName)) {
+                return sec.lowerCaseName.length();
+            }
+            if (s.startsWith(sec.abreviation)) {
+                return sec.abreviation.length();
+            }
+            if (sec.alternateAbreviation != null && s.startsWith(sec.lowerCaseName)) {
+                return sec.alternateAbreviation.length();
+            }
+        }
+        return 0;
     }
 
+    private Section(String abreviation) {
+        lowerCaseName = name().toLowerCase();
+        this.abreviation = abreviation;
+    }
+
+    private Section(String abreviation, String alternateAbreviation) {
+        this(abreviation);
+        this.alternateAbreviation = alternateAbreviation;
+    }
+
+    private String lowerCaseName;
     private String abreviation;
+    private String alternateAbreviation;
+    public static final int maxLength = 10;    //  fixme: compute
 }
--- src/main/java/com/bsteele/bsteeleMusicApp/client/Song.java
+++ src/main/java/com/bsteele/bsteeleMusicApp/client/Song.java
@@ -12,145 +12,109 @@ import jsinterop.annotations.JsType;
  */
 @JsType
 public class Song {
-    
+
     public String transcribe(String chords, int halfSteps) {
         GWT.log("Song.transcribe()  here: " + halfSteps + " to: " + chords);
 
         int chordNumber = 0;
         String sout = "";
         int state = 0;
-        for (int ci = 0; ci < chords.length(); ci++) {
-            char c = chords.charAt(ci);
-            switch (state) {
-                case 1:	//  chord symbol modifier, one character only
-                    state = 0;
-                    if (c == 'b' || c == js_flat) {
-                        chordNumber -= 1;
+        String chordLines[] = chords.split("\n");
+        for (int li = 0; li < chordLines.length; li++) {
+            String chordLine = chordLines[li];
+            state = 0;
+            
+            for (int ci = 0; ci < chordLine.length(); ci++) {
+                char c = chordLine.charAt(ci);
+                switch (state) {
+                    case 1:	//  chord symbol modifier, one character only
+                        state = 0;
+                        if (c == 'b' || c == js_flat) {
+                            chordNumber -= 1;
+                            sout += chordNumberToLetter(chordNumber + halfSteps);
+                            break;
+                        }
+                        if (c == '#' || c == js_sharp) {
+                            chordNumber += 1;
+                            sout += chordNumberToLetter(chordNumber + halfSteps);
+                            break;
+                        }
+                        if (c == js_natural) {
+                            sout += chordNumberToLetter(chordNumber + halfSteps);
+                            break;
+                        }
                         sout += chordNumberToLetter(chordNumber + halfSteps);
+                    //	fall through
+                    default:
+                    case 0:
+                        if (c == '(') {
+                            sout += c;
+                            state = 11;
+                            break;
+                        }
+
+                        //  don't transcribe the section identifiers that happen to look like notes
+                        String toMatch = chordLine.substring(ci, Math.min(chordLine.length() - ci, Section.maxLength));
+                        int matchLength = Section.matchLength(toMatch);
+                        if (matchLength > 0 && chordLine.charAt(ci + matchLength) == ':') {
+                            String sub = chordLine.substring(ci, matchLength);
+                            sout += sub;
+                            ci += matchLength - 1; //  skip the end of the section id
+                            break;
+                        }
+
+                        if (c >= 'A' && c <= 'G') {
+                            chordNumber = chordLetterToNumber(c);
+                            state = 1;
+                            break;
+                        }
+                        if (toMatch.startsWith("maj")) {
+                            sout += "maj";
+                            ci += 2;
+                        } else if (toMatch.startsWith("sus")) {
+                            sout += "sus";
+                            ci += 2;
+                        } else if ((c >= '0' && c <= '9')
+                                || c == 'm'
+                                || c == ' ' || c == '-' || c == '|' || c == '/'
+                                || c == '\n'
+                                || c == js_delta) {
+                            sout += c;
+                        } else {	//  don't parse the rest
+                            sout += c;
+                            state = 10;
+                        }
                         break;
-                    }
-                    if (c == '#' || c == js_sharp) {
-                        chordNumber += 1;
-                        sout += chordNumberToLetter(chordNumber + halfSteps);
-                        break;
-                    }
-                    if (c == js_natural) {
-                        sout += chordNumberToLetter(chordNumber + halfSteps);
-                        break;
-                    }
-                    sout += chordNumberToLetter(chordNumber + halfSteps);
-                //	fall through
-                default:
-                case 0:
-                    if (c == '(') {
+
+                    case 10: //	wait for newline
                         sout += c;
-                        state = 11;
                         break;
-                    }
 
-                    //  don't transcribe the section identifiers that happen to look like notes
-//                    String sub = chords.substring(ci, 5);
-//                    if ( / ^ Coda:/i.test(sub)
-//                     
-//                     
-//                        ) {
-//                                sout += sub;
-//                        ci += sub.length - 1; //  skip the end of the section id
-//                        break;
-//                    }
-//                    sub = chords.substr(ci, 4);
-//                    if ( / ^ Out:/i.test(sub)
-//                     
-//                     
-//                        ) {
-//                                sout += sub;
-//                        ci += sub.length - 1; //  skip the end of the section id
-//                        break;
-//                    }
-//                    sub = chords.substr(ci, 3);
-//                    if ( / ^ Ch:/i.test(sub) ||  / ^ Co
-//                    :/i.test(sub) ||  / ^ C
-//                    :/i.test(sub) ||  / ^ Br
-//                    :/i.test(sub) ||  / ^ Pc
-//                    :/i.test(sub)
-//                     
-//                     
-//                        ) {
-//                                sout += sub;
-//                        ci += sub.length - 1; //  skip the end of the section id
-//                        break;
-//                    }
-//                    sub = sub.substr(ci, 2);
-//                    if ( / ^ C:/i.test(sub)
-//                     
-//                     
-//                        ) {
-//                                sout += sub;
-//                        ci += sub.length - 1; //  skip the end of the section id
-//                        break;
-//                    }
-//
-//                    if (c >= 'A' && c <= 'G') {
-//                        chordNumber = chordLetterToNumber(c);
-//                        state = 1;
-//                        break;
-//                    }
-//
-//                    if (chords.startsWith('maj'
-//                    , ci
-//                     
-//                     
-//                        )) {
-//                                sout += 'maj'
-//                        ;
-//                                ci += 2;
-//                    }
-//                    else if (chords.startsWith(
-//                    
-//                    'sus'
-//                    , ci
-//                     
-//                     
-//                        )) {
-//                                sout += 'sus'
-//                        ;
-//                                ci += 2;
-//                    }
-//                    else 
-                        if ((c >= '0' && c <= '9')
-                            || c == 'm'
-                            || c == ' ' || c == '-' || c == '|' || c == '/'
-                            || c == '\n'
-                            || c == js_delta) {
-                                sout += c;
-                            } 
-                                else {	//  don't parse the rest
-                                sout += c;
-                                state = 10;
-                            }
-                    break;
-
-                case 10: //	wait for newline
-                    sout += c;
-                    if (c == '\n') {
-                        state = 0;
-                    }
-                    break;
-
-                case 11: //	wait for newline or closing paren
-                    sout += c;
-                    if (c == '\n' || c == ')') {
-                        state = 0;
-                    }
-                    break;
+                    case 11: //	wait for newline or closing paren
+                        sout += c;
+                        if (c == ')') {
+                            state = 0;
+                        }
+                        break;
+                }
             }
+            sout += '\n';
         }
-        
+
         GWT.log(sout);
         return sout;
     }
 
-    private final String chordNumberToLetter(int n) {
+    private int chordLetterToNumber(char letter) {
+        int i = letter - 'A';
+        //                            a  a# b  c  c# d  d# e  f f#  g  g#
+        //                            0  1  2  3  4  5  6  7  8  9  10 11
+
+        return chordLetterToNumber[i];
+    }
+    private static final int chordLetterToNumber[] = new int[]{0, 2, 3, 5, 7, 8, 10};
+
+    private String chordNumberToLetter(int n) {
 
         n = n % 12;
         if (n < 0) {
