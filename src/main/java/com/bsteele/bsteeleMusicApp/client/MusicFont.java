c6b23f28c65e81b349331dbb620e231152fd3e19
pearlbob <bob@bsteele.com>
2018-02-07 14:26:26 -0800

    corrected oversight of not loading lyrics songs from the cloud

---
+++ src/main/java/com/bsteele/bsteeleMusicApp/client/MusicFont.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2018 Robert Steele at bsteele.com
+ */
+package com.bsteele.bsteeleMusicApp.client;
+
+/**
+ *
+ * @author bob
+ */
+public class MusicFont {
+    
+    MusicFont(){
+        
+    }
+    
+}
