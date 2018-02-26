0db1a04fa14f9d413bb48eb73df422cf5583e3b7
pearlbob <bob@bsteele.com>
2018-02-26 12:44:41 -0800

    SongListPresenterWidget functional with songs

--- src/main/java/com/bsteele/bsteeleMusicApp/client/presenterWidgets/SongListPresenterWidget.java
+++ src/main/java/com/bsteele/bsteeleMusicApp/client/presenterWidgets/SongListPresenterWidget.java
@@ -3,43 +3,112 @@
  */
 package com.bsteele.bsteeleMusicApp.client.presenterWidgets;
 
+import com.bsteele.bsteeleMusicApp.client.resources.AppResources;
+import com.bsteele.bsteeleMusicApp.shared.Song;
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONValue;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.PresenterWidget;
 import com.gwtplatform.mvp.client.View;
+import java.util.ArrayList;
+import java.util.TreeSet;
 
 /**
  *
  * @author bob
  */
 public class SongListPresenterWidget extends PresenterWidget<SongListPresenterWidget.MyView> {
-    public interface MyView extends View 
-            //implements DisplayMessageEvent.DisplayMessageHandler
-    {
-        void displayCurrentUserName(String username);
-    }
 
-    //private final CurrentUserService currentUserService;
+  public interface MyView extends View //implements DisplayMessageEvent.DisplayMessageHandler
+  {
 
-    @Inject
-    SongListPresenterWidget(final EventBus eventBus,
-                    final MyView view
-            //, CurrentUserService currentUserService
-    ) {
-        super(eventBus, view);
+    void displayCurrentUserName(String username);
 
-        //this.currentUserService = currentUserService;
-    }
-    
+    Grid getSongGrid();
+
+    TextBox getSongSearch();
+  }
+
+  //private final CurrentUserService currentUserService;
+  @Inject
+  SongListPresenterWidget(final EventBus eventBus,
+          final MyView view
+  //, CurrentUserService currentUserService
+  ) {
+    super(eventBus, view);
+
+    //this.currentUserService = currentUserService;
+  }
+
 //    @Override
 //    public void onDisplayMessage(DisplayMessageEvent event) {
 //        getView().addMessage(event.getMessage());
 //    }
+  @Override
+  public void onBind() {
+    //addRegisteredHandler(DisplayMessageEvent.getType(), this);
+    getView().displayCurrentUserName("bob"//currentUserService.getCurrentUsername()
+    );
 
-    @Override
-    public void onBind() {
-      //addRegisteredHandler(DisplayMessageEvent.getType(), this);
-        getView().displayCurrentUserName("bob"//currentUserService.getCurrentUsername()
-        );
+    {
+      String jsonString = AppResources.INSTANCE.allSongsAsJsonString().getText();
+      if (jsonString != null && jsonString.length() > 0) {
+        JSONValue jv = JSONParser.parseStrict(jsonString);
+        if (jv != null) {
+          JSONArray ja = jv.isArray();
+          if (ja != null) {
+            int jaLimit = ja.size();
+            for (int i = 0; i < jaLimit; i++) {
+              Song song = Song.songFromJsonObject(ja.get(i).isObject());
+              if (song != null) {
+                songs.add(song);
+              }
+            }
+          }
+        }
+      }
     }
+
+    searchSongs(null);
+
+    TextBox songSearch = getView().getSongSearch();
+    songSearch.addKeyUpHandler((event) -> {
+      searchSongs(songSearch.getValue());
+    });
+
+    songSearch.setFocus(true);
+  }
+
+  private void searchSongs(String search) {
+    if (search == null) {
+      search = "";
+    }
+    search = search.replaceAll("[^\\w\\s']+", "");
+    search = search.toLowerCase();
+    TreeSet<Song> sortedSongs = new TreeSet<>();
+    for (Song song : songs) {
+      if (search.length() == 0
+              || song.getTitle().toLowerCase().contains(search)
+              || song.getArtist().toLowerCase().contains(search)) {
+        sortedSongs.add(song);
+      }
+    }
+    Grid songGrid = getView().getSongGrid();
+    songGrid.resize(sortedSongs.size(), columns);
+    {
+      int r = 0;
+      for (Song song : sortedSongs) {
+        songGrid.setHTML(r, 0, song.getTitle());
+        songGrid.setHTML(r, 1, song.getArtist());
+        r++;
+      }
+    }
+  }
+
+  private static final int columns = 2;
+  private final ArrayList<Song> songs = new ArrayList<>();
 }
--- src/main/java/com/bsteele/bsteeleMusicApp/client/presenterWidgets/SongListView.java
+++ src/main/java/com/bsteele/bsteeleMusicApp/client/presenterWidgets/SongListView.java
@@ -6,6 +6,8 @@ package com.bsteele.bsteeleMusicApp.client.presenterWidgets;
 import com.google.gwt.dom.client.SpanElement;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
 import javax.inject.Inject;
@@ -17,12 +19,18 @@ import javax.inject.Inject;
 public class SongListView
         extends ViewImpl
         implements SongListPresenterWidget.MyView {
-  
+
   interface Binder extends UiBinder<Widget, SongListView> {
   }
 
   @UiField
   SpanElement username;
+
+  @UiField
+  TextBox songSearch;
+
+  @UiField
+  Grid songGrid;
 
   @Inject
   SongListView(Binder binder) {
@@ -32,5 +40,15 @@ public class SongListView
   @Override
   public void displayCurrentUserName(String username) {
     this.username.setInnerHTML(username);
+  }
+
+  @Override
+  public Grid getSongGrid() {
+    return songGrid;
+  }
+
+  @Override
+  public TextBox getSongSearch() {
+    return songSearch;
   }
 }
