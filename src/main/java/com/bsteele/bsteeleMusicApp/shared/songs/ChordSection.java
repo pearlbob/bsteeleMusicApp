803a1ca6895b4ac47635ce73bddfe63e449535ee
pearlbob <bob@bsteele.com>
2019-03-09 09:05:51 -0800

    commit for deploy at 2019-03-09T09:05:51-08:00

--- src/main/java/com/bsteele/bsteeleMusicApp/shared/songs/ChordSection.java
+++ src/main/java/com/bsteele/bsteeleMusicApp/shared/songs/ChordSection.java
@@ -313,27 +313,6 @@ public class ChordSection extends MeasureNode implements Comparable<ChordSection
      * negative integer, zero, or a positive integer as this object is less
      * than, equal to, or greater than the specified object.
      *
-     * <p>The implementor must ensure <tt>sgn(x.compareTo(y)) ==
-     * -sgn(y.compareTo(x))</tt> for all <tt>x</tt> and <tt>y</tt>.  (This
-     * implies that <tt>x.compareTo(y)</tt> must throw an exception iff
-     * <tt>y.compareTo(x)</tt> throws an exception.)
-     *
-     * <p>The implementor must also ensure that the relation is transitive:
-     * <tt>(x.compareTo(y)&gt;0 &amp;&amp; y.compareTo(z)&gt;0)</tt> implies
-     * <tt>x.compareTo(z)&gt;0</tt>.
-     *
-     * <p>Finally, the implementor must ensure that <tt>x.compareTo(y)==0</tt>
-     * implies that <tt>sgn(x.compareTo(z)) == sgn(y.compareTo(z))</tt>, for
-     * all <tt>z</tt>.
-     *
-     * <p>Note: this class has a natural ordering that is
-     * inconsistent with equals.
-     *
-     * <p>In the foregoing description, the notation
-     * <tt>sgn(</tt><i>expression</i><tt>)</tt> designates the mathematical
-     * <i>signum</i> function, which is defined to return one of <tt>-1</tt>,
-     * <tt>0</tt>, or <tt>1</tt> according to whether the value of
-     * <i>expression</i> is negative, zero or positive.
      *
      * @param o the object to be compared.
      * @return a negative integer, zero, or a positive integer as this object
