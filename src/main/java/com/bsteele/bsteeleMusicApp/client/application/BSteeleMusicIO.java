6f01a151b2e53b594516c4ec148e906d7fe47df0
pearlbob <bob@bsteele.com>
2020-07-09 14:28:43 -0700

    commit for deploy at 2020-07-09T14:28:43-07:00

--- src/main/java/com/bsteele/bsteeleMusicApp/client/application/BSteeleMusicIO.java
+++ src/main/java/com/bsteele/bsteeleMusicApp/client/application/BSteeleMusicIO.java
@@ -4,6 +4,7 @@ import com.bsteele.bsteeleMusicApp.client.Function;
 import com.bsteele.bsteeleMusicApp.client.SongPlayMaster;
 import com.bsteele.bsteeleMusicApp.client.jsTypes.WebSocket;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import jsinterop.annotations.JsType;
 
@@ -25,26 +26,34 @@ public class BSteeleMusicIO {
     public BSteeleMusicIO(SongPlayMaster songPlayMaster) {
 
         this.songPlayMaster = songPlayMaster;
-        String url = getWebSocketURL();
+        url = getWebSocketURL();
         if (url != null) {
-           logger.fine("websocket url: " + url);
+            logger.fine("websocket url: " + url);
 
-            socket = new WebSocket(url);
-            socket.onerror = new SocketErrorFunction();
-            socket.onmessage = new SocketReceiveFunction();
-        }
+            confirmSocketConnection();
+        } else
+            logger.warning("Cannot create websocket URL!");
 
         songPlayMaster.setBSteeleMusicIO(this);
 
         songPlayMaster.initialize();
+
+        scheduler.scheduleFixedDelay(new Scheduler.RepeatingCommand() {
+            @Override
+            public boolean execute() {
+                confirmSocketConnection();
+                return true;
+            }
+        }, 15 * 1000);
     }
 
-    private String getWebSocketURL() {
+    private static String getWebSocketURL() {
         String url = GWT.getHostPageBaseURL();
         logger.fine("raw base url: " + url);
         if (url == null
-          //  || url.startsWith("http://127.0.0.1:8888/")
+                //  || url.startsWith("http://127.0.0.1:8888/")
                 || url.startsWith("file://")
+                || url.contains("bsteele.com")  //  not expected from internet web server, only local
         )
             return null;
 
@@ -66,7 +75,7 @@ public class BSteeleMusicIO {
 
             OnMessageEvent me = (OnMessageEvent) event;
 
-            logger.fine("message recv: " + me.data +" at " + dateTimeFormat.format(new Date(t)));
+            logger.fine("message recv: " + me.data + " at " + dateTimeFormat.format(new Date(t)));
             songPlayMaster.onMessage(t / 1000.0, me.data);
             return event;
         }
@@ -77,14 +86,13 @@ public class BSteeleMusicIO {
         @Override
         public Object call(Object event) {
             if (socket != null)
-                logger.info("warning: socket readyState: " + socket.readyState);
-            isSocketOpen = isSocketOpen();
+                logger.info("SocketErrorFunction: socket readyState: " + WebSocketReadyStateNameAt(socket.readyState));
             return event;
         }
     }
 
     public boolean isSocketOpen() {
-        return socket != null && socket.readyState == 1;
+        return socket != null && socket.readyState == WebSocketReadyState.OPEN.ordinal();
     }
 
     @JsType(isNative = true, name = "Object", namespace = GLOBAL)
@@ -109,17 +117,55 @@ public class BSteeleMusicIO {
             return false;
         }
 
-        logger.fine("socket send: " + message.substring(0,Math.min(30,message.length()))
+        logger.fine("socket send: " + message.substring(0, Math.min(30, message.length()))
                 + " at " + dateTimeFormat.format(new Date()));
 
         socket.send(message);
         return true;
     }
 
+    private void confirmSocketConnection() {
+        if (isSocketOpen() || url == null) {
+            if (!wasConnected) {
+                logger.info("confirmSocketConnection() connected");
+                wasConnected = true;
+            }
+            return;
+        }
+        wasConnected = false;
+        if (url == null) {
+            return;
+        }
+
+        //  try to open a new one
+        socket = new WebSocket(url);
+        socket.onerror = new SocketErrorFunction();
+        socket.onmessage = new SocketReceiveFunction();
+
+        logger.info("confirmSocketConnection() reset");
+    }
+
+    enum WebSocketReadyState {
+        CONNECTING, // Socket has been created.The connection  yet open.
+        OPEN,       // The connection is open and ready to  communicate.
+        CLOSING,    // The connection is in the process of closing.
+        CLOSED      // The connection is closed or couldn't be opened.
+    }
+
+    String WebSocketReadyStateNameAt(int i) {
+        for (WebSocketReadyState ws : WebSocketReadyState.values()) {
+            if (i == ws.ordinal())
+                return ws.toString();
+        }
+        return Integer.toString(i);
+    }
+
+    private final String url;
     private final SongPlayMaster songPlayMaster;
     private static WebSocket socket;
-    private static boolean isSocketOpen = true;
-    private final DateTimeFormat dateTimeFormat =  DateTimeFormat.getFormat("HH:mm:ss.SSS\"");
+    private boolean wasConnected = false;
+    private final DateTimeFormat dateTimeFormat = DateTimeFormat.getFormat("HH:mm:ss.SSS\"");
+    private static final Scheduler scheduler = Scheduler.get();
 
     private static final Logger logger = Logger.getLogger(BSteeleMusicIO.class.getName());
 }
